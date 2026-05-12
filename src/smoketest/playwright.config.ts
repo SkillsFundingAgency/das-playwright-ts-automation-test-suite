e1fd6ff81e47e2de3c73c38cf0845581aa6b201a
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2026-05-12 15:25:35 +0100

    added CI env in yml

--- src/smoketest/playwright.config.ts
+++ src/smoketest/playwright.config.ts
@@ -40,11 +40,9 @@ export default defineConfig({
   workers: process.env.CI ? 2 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
-    ['html'],
-    ['junit', { outputFile: 'results.xml' }],   
     ['list'], 
+    ['junit', { outputFile: 'results.xml' }],   
     ['allure-playwright']
-
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
